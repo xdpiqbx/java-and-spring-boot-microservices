911c56ee8ae7dbccfd1e0486a416bdcff3672588
xdpiqbx <xdpiqbx@gmail.com>
2023-03-04 16:23:53 +0200

    added fraud

---
+++ fraud/src/main/java/com/dpiqb/FraudApp.java
@@ -0,0 +1,11 @@
+package com.dpiqb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApp {
+  public static void main(String[] args) {
+    SpringApplication.run(FraudApp.class, args);
+  }
+}
