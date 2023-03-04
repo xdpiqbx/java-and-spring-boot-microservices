0b030202043c622820cfd5386df2324d9b967685
xdpiqbx <xdpiqbx@gmail.com>
2023-03-04 20:44:57 +0200

    all works.. next step Spring Cloud Sleuth

--- fraud/src/main/java/com/dpiqb/FraudController.java
+++ fraud/src/main/java/com/dpiqb/FraudController.java
@@ -1,16 +1,19 @@
 package com.dpiqb;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public class FraudController {
   private final FraudCheckService fraudCheckService;
   @GetMapping(path = "{customerId}")
   public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
     boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+    log.info("Fraud check request for customer {}", customerId);
     return new FraudCheckResponse(isFraudulentCustomer);
   }
 }
